e8eacb0da168251662d1ef8ec5faad16de11072b
funny family <maksvadik228@gmail.com>
2021-10-06 22:57:13 +0700

    update global type declaration

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,22 +1,16 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
-// ?????
-// declare module "*.css"; "*.scss"; {
-//   const classes: { readonly [key: string]: string };
-//   export default classes;
-// }
-
-declare module "*.css" {
+declare module '*.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
 
-declare module "*.scss" {
+declare module '*.scss' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
