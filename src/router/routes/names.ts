c66d0d99557c5b20a6fe2ffdd1b46863d73003f2
funny family <maksvadik228@gmail.com>
2021-10-31 02:43:45 +0700

    update name of routes

--- src/router/routes/names.ts
+++ src/router/routes/names.ts
@@ -1,7 +1,10 @@
 export const names = {
-  home: "home",
-  about: "about",
-  my: "my",
-  profile: "profile",
-  chats: "chats",
+  home: 'home',
+  about: 'about',
+  my: 'my',
+  profile: 'profile',
+  chats: 'chats',
+  notFound: 'notFound'
 };
+
+export type Names = keyof typeof names;
