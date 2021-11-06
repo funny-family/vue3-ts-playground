da48c606741b1b98fc9e783063680c7baeadf936
funny family <maksvadik228@gmail.com>
2021-11-06 15:19:58 +0700

    update main file of app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { createApp } from "vue";
-// import App from "./App.vue";
-import App from "@/App.vue";
-import { router } from "./router";
-import { store } from "./store";
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import { router } from './router';
+import { store } from './store';
+import { hideConsoleLogs } from './utils/hide-console-logs';
+import type { EnvironmentVariable } from '@/types';
+import { extractFromEnv } from './utils/extract-from-env';
+
+const environmentVariable = extractFromEnv(process.env.NODE_ENV) as EnvironmentVariable;
+if (environmentVariable === 'production') {
+  hideConsoleLogs();
+}
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
