a06b7545cd0deac86a0c6221f36832bdc2b82ebb
funny family <maksvadik228@gmail.com>
2021-11-13 01:36:09 +0700

    update styles of "gif-404" component

--- src/app/shared/components/gif-404/styles/gif-404.styles.ts
+++ src/app/shared/components/gif-404/styles/gif-404.styles.ts
@@ -1,13 +1,3 @@
-// import styles from './gif-404.module.scss';
-// import styles from './gif-404.scss';
-
-// export { styles };
-
-// import baseStyles from './gif-404.base.scss';
-// import desktopStyles from './gif-404.desktop.scss';
-
-// export const styles = { ...baseStyles, ...desktopStyles };
-
 /**
  * @see https://habr.com/ru/company/constanta/blog/428800/ !!!!!
  * "Готовим идеальный CSS"
@@ -16,7 +6,6 @@
  * читать до "Убираем переменную рантайма" ДАЛЬШЕ НИ ШАГУ!!!!
  */
 
-import styles from './gif-404.styles.module.scss';
-// import styles from './gif-404.styles.scss';
+import styles from './gif-404.styles.scss';
 
 export { styles };
