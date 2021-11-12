72d2490015cb351939dd209ba837e147e4116b88
funny family <maksvadik228@gmail.com>
2021-11-13 02:03:21 +0700

    update styles of "gif-404" component

--- src/app/shared/components/gif-404/styles/gif-404.styles.ts
+++ src/app/shared/components/gif-404/styles/gif-404.styles.ts
@@ -6,6 +6,8 @@
  * читать до "Убираем переменную рантайма" ДАЛЬШЕ НИ ШАГУ!!!!
  */
 
+//  https://github.com/Quramy/typed-css-modules
+
 import styles from './gif-404.styles.scss';
 
 export { styles };
