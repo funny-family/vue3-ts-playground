3ecfa0bd7c1d9b6a999dda9fbc20075de7ac4e4f
funny family <maksvadik228@gmail.com>
2021-11-28 18:18:18 +0700

    update "nameOf" function

--- src/app/shared/utils/name-of.ts
+++ src/app/shared/utils/name-of.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-escape, @typescript-eslint/ban-types */
 /**
  * @see https://stackoverflow.com/questions/4602141/variable-name-as-a-string-in-javascript
+ * @description get name of variable
  */
 export const nameOf = (callback: Function) => callback.toString().replace(/[ |\(\)=>]/g, '');
