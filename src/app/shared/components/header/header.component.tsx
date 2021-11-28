b56efe095ae0818375cfdc80ac25e14395711e24
funny family <maksvadik228@gmail.com>
2021-11-28 18:41:30 +0700

    update "header" component

--- src/app/shared/components/header/header.component.tsx
+++ src/app/shared/components/header/header.component.tsx
@@ -1,26 +1,30 @@
-import { defineComponent, PropType } from "vue";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { defineComponent, getCurrentInstance, withScopeId } from 'vue';
 
-// import { setup } from "./header.setup";
-// import { props } from "./header.props";
+// import { setup } from './header.setup';
+// import { props } from './header.props';
 
 export const Header = defineComponent({
-  name: "Header",
-  // props,
+  name: 'Header',
   props: {
     title: {
-      default: "this is main heading!",
+      default: 'this is main heading!',
       required: true,
-      type: String,
-    },
+      type: String
+    }
   },
-  // setup,
+  setup() {
+    const instance = getCurrentInstance();
+    // @ts-ignore
+    const scopeId = instance?.type.__scopeId;
+    const withId = withScopeId(scopeId);
+
+    console.log('scopeId:', scopeId);
+    console.log('withId:', withId);
+  },
   render() {
-    return (
-      <header style={{ height: '60px', backgroundColor: 'red' }}>
-        {this.$props.title}
-      </header>
-    );
+    return <header style={{ height: '60px', backgroundColor: 'red' }}>{this.$props.title}</header>;
   }
 });
 
-export default Header;
+// https://github.com/wonderful-panda/vue-tsx-support#migration-from-v2
