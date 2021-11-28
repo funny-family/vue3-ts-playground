6c5e10e6757a41bcd1866371b9625938f91b413d
funny family <maksvadik228@gmail.com>
2021-11-28 18:41:07 +0700

    update "not-found" component

--- src/app/pages/not-found/not-found.component.tsx
+++ src/app/pages/not-found/not-found.component.tsx
@@ -3,7 +3,7 @@
 // https://programming.vip/docs/vue-3-props-emit-slot-render-jsx-and-createelement.html
 
 import { defineComponent, ref, withScopeId } from 'vue';
-// import { RouterView, RouterLink } from "vue-router"
+import { RouterLink } from 'vue-router';
 import { Header } from '../../shared/components/header/header.component';
 import { Gif404 } from '../../shared/components/gif-404/gif-404.component';
 import { Govno } from '../../shared/components/gif-404';
@@ -31,12 +31,17 @@ export const NotFound = defineComponent({
 
     console.log('isGif404Visible:', isGif404Visible.value);
     console.log('isRed:', isRed);
+    const val = ref('');
 
     const componentId = generateGuid();
     const withId = withScopeId(componentId);
 
     return withId(() => (
       <div class="gif404">
+        <RouterLink to="/">
+          <h2>to home!</h2>
+        </RouterLink>
+
         <Header title="t45672" />
 
         <h1 class={classNames(isRed && 'is-red')}>{title}</h1>
@@ -44,9 +49,18 @@ export const NotFound = defineComponent({
 
         {/* @ts-ignore */}
         {/* <div v-if={isGif404Visible.value}> */}
-        <Gif404 title="dadada" aria-label="this is giiifff!" />
+        <Gif404
+          onClick={(e) => {
+            console.log(e)
+          }}
+          style={{ color: 'red' }}
+          class="121212112"
+          title="this is title!"
+        />
 
-        <Govno.Gif404 title="11" />
+        <hr />
+
+        <Govno.Gif404 class="3434343343" title="11" />
         {/* </div> */}
         {/* {isGif404Visible.value === true &&
           <Gif404 v-if={() => true} aria-label="this is giiifff!" />
