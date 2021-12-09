d6592f3c1c33706cf271a4c92fa543e32bd7d1b5
funny family <maksvadik228@gmail.com>
2021-12-09 23:56:05 +0700

    update global types

--- src/app/shared/types/index.ts
+++ src/app/shared/types/index.ts
@@ -9,6 +9,12 @@ export type RecordPropsDefinition<P = Data> = {
   [K in keyof P]: Prop<P[K]>; // [K in keyof P]: PropValidator<P[K]> | null; <- "null" makes the type work incorrectly
 };
 
+export type CustomSlot<A, T> = ((...args: A[]) => T) | undefined;
+
+export type DefaultSlot<T, A = undefined> = {
+  default: CustomSlot<A, T>;
+};
+
 export interface SetupCtx<E = EmitsOptions, A = Data, S = Slots>
   extends Omit<SetupContext<E>, 'attrs' | 'slots'> {
   attrs: A;
