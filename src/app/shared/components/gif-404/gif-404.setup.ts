d8f0e20ec1071906a75074a94bd66302e5ec89f3
funny family <maksvadik228@gmail.com>
2021-12-12 14:24:43 +0700

    clean up

--- src/app/shared/components/gif-404/gif-404.setup.ts
+++ src/app/shared/components/gif-404/gif-404.setup.ts
@@ -3,13 +3,7 @@
 // separate setup????? https://github.com/vuejs/composition-api/issues/650
 // https://mrcrmn.dev/articles/lightweight-components-with-vue3-and-jsx/
 
-import {
-  EmitsOptions,
-  getCurrentInstance,
-  ref,
-  SetupContext,
-  withScopeId
-} from 'vue';
+import { EmitsOptions, ref, SetupContext, withScopeId } from 'vue';
 import type { Gif404Props, Gif404Attrs } from './gif-404.props';
 import { generateGuid } from '@/app/shared/utils/guid';
 import type {
@@ -19,11 +13,6 @@ import type {
 } from '@/app/shared/types';
 import type { Gif404Slots } from './gif-404.slots';
 
-// export const setup = (
-//   props: Readonly<Gif404Props>,
-//   context: SetupCtx<Gif404Emits, Gif404Attrs>
-// ) => {
-/* ----------------------------------------------------------------------------------- */
 export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
   const props = p as Readonly<Gif404Props>;
   const context = ctx as unknown as SetupCtx<
@@ -31,12 +20,6 @@ export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
     HTMLAttributesWithoutCSSClass<Gif404Attrs> & CSSClassAsString,
     Gif404Slots.VNodeList
   >;
-  /* ----------------------------------------------------------------------------------- */
-  // export const setup = (
-  //   props: Readonly<Gif404Props>,
-  //   context: SetupCtx<Gif404Emits, Gif404Attrs, Gif404Slots.VNodeList>
-  // ) => {
-  /* ----------------------------------------------------------------------------------- */
 
   const title = 'Not found page!';
   const isImageVisible = ref(true);
@@ -44,19 +27,6 @@ export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
   console.log('Gif404 context:', context.attrs);
 
   const c = context.attrs.class;
-
-  const instance = getCurrentInstance();
-  // console.log('uid:', instance?.uid);
-
-  // console.log(
-  //   getNormalizedRouteRecordByName({
-  //     availableName: 'notFound'
-  //   })
-  // );
-
-  // https://zhuanlan.zhihu.com/p/267343741
-
-  // const scopeId = instance?.type.__isBuiltIn;
 
   const componentId = generateGuid();
   const withId = withScopeId(componentId);
@@ -72,5 +42,3 @@ export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
 };
 
 export type Gif404Bindings = ReturnType<typeof setup>;
-
-// export type RawBindings = ReturnType<typeof setup>;
