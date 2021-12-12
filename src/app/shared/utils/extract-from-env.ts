dc21ae760a8ee186965eab464468939971639de3
funny family <maksvadik228@gmail.com>
2021-12-12 17:18:08 +0700

    update "extract-from-env" util function

--- src/app/shared/utils/extract-from-env.ts
+++ src/app/shared/utils/extract-from-env.ts
@@ -1,7 +1,9 @@
-export const extractFromEnv = <T extends string>(envVariable: T | undefined): T => {
+export const extractFromEnv = <T extends string>(
+  envVariable: string | undefined
+): T => {
   if (!envVariable) {
     throw new Error(`Env variable does not exist!`);
   }
 
-  return envVariable;
+  return envVariable as T;
 };
