5d7e312ed7382bd3832744e213883d544572cb7e
funny family <maksvadik228@gmail.com>
2021-12-12 17:18:43 +0700

    update "name-of" util function

--- src/app/shared/utils/name-of.ts
+++ src/app/shared/utils/name-of.ts
@@ -1,6 +1,18 @@
-/* eslint-disable no-useless-escape, @typescript-eslint/ban-types */
+/* eslint-disable no-useless-escape */
+
+// /function{return(.+)(;?)}/
+
+// https://mothereff.in/js-variables
+
 /**
  * @see https://stackoverflow.com/questions/4602141/variable-name-as-a-string-in-javascript
  * @description get name of variable
  */
-export const nameOf = (callback: Function) => callback.toString().replace(/[ |\(\)=>]/g, '');
+export const nameOf = (callback: Function) =>
+  callback
+    .toString()
+    .replace(/(\r\n|\n|\r)/gm, '')
+    .replace(/[ |\(\)=>]/g, '')
+
+    .replace(/function{return/g, '')
+    .replace(/(;?)}/g, '');
