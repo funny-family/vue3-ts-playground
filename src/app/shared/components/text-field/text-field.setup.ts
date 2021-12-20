0bb6dcb9796c60c820b1fa84612e1467cc9a8763
funny family <maksvadik228@gmail.com>
2021-12-20 21:34:11 +0700

    update layout of "text-field" componenty

--- src/app/shared/components/text-field/text-field.setup.ts
+++ src/app/shared/components/text-field/text-field.setup.ts
@@ -5,12 +5,13 @@ import type {
   SetupCtx
 } from '@/app/shared/types';
 import type { TextFieldAttrs, TextFieldProps } from './text-field.props';
-import type { TextFieldEmits } from './text-field.emits';
+import { emitNames, TextFieldEmits } from './text-field.emits';
 
 export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
   const props = p as Readonly<TextFieldProps>;
   const context = ctx as unknown as SetupCtx<
-    TextFieldEmits,
+    // EmitsOptions,
+    TextFieldEmits.AsFunctionArguments,
     HTMLAttributesWithoutCSSClass<TextFieldAttrs> & CSSClassAttribute
   >;
 
@@ -21,7 +22,7 @@ export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
     // console.log('"TextField" value inside:', inputValue);
     console.log('"TextField" event inside:', event);
 
-    context.emit('input', event);
+    context.emit(emitNames.input, event);
   };
 
   return {
