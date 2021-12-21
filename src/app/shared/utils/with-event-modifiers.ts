1e004d0cfbb4bec02749e61b3efd2576b8e84f8b
funny-family <maksvadik228@gmail.com>
2021-12-21 21:42:16 +0700

    add incomplete "withEventModifiers" util function

---
+++ src/app/shared/utils/with-event-modifiers.ts
@@ -0,0 +1,49 @@
+/* eslint-disable */
+import { Events, HTMLAttributes , withModifiers } from 'vue';
+import type { ArrayElement } from '../types';
+import { enumify } from './enumify';
+
+// type EventHandlers<E> = {
+//   [K in keyof E]?: E[K] extends Function ? E[K] : (payload: E[K]) => void
+// }
+
+// type O = {
+//   [eventFunction: string]: (event: Event) => void;
+// };
+
+// const o: O = {
+//   onCLick: (e) => {}
+// }
+
+type EventModifierList = [
+  'stop',
+  'prevent',
+  'capture',
+  'self',
+  'once',
+  'passive'
+];
+
+type EventModifiers = ArrayElement<EventModifierList>;
+
+/**
+ * @see https://v3.vuejs.org/guide/events.html#event-modifiers
+ */
+const eventModifierList: EventModifierList = [
+  'stop',
+  'prevent',
+  'capture',
+  'self',
+  'once',
+  'passive',
+];
+
+const eventModifiers = enumify<EventModifiers>(eventModifierList);
+
+export const withEventModifiers = (objectWithEventFunction: Events, modifiers: string[]): any => {
+  /**
+   * eventFunction = { onClick: (event: Event): void => {} }
+   */
+  // const htmlAttributeKeys = Object.keys(htmlAttributes);
+};
+
