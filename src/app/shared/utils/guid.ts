ff9408631019a4c031bb8a334835a7250d388830
funny family <maksvadik228@gmail.com>
2021-12-22 22:39:58 +0700

    update guid

--- src/app/shared/utils/guid.ts
+++ src/app/shared/utils/guid.ts
@@ -1,13 +1,24 @@
+/**
+ * @description
+ * Generate pseudo random id.
+ *
+ * @example
+ * const guid = generateGuid();
+ * console.log(guid); // 8a878a87-0e55-0e55-0e55-0e556bbf6bbf
+ */
 export const generateGuid = (): string => {
   // eslint-disable-next-line no-bitwise
-  const S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  const guid = `
+  const S4 = () =>
+    (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+
+  const withoutEscapeCharacters = (string: string): string =>
+    string.replace(/\r?\n|\r/g, ' ').replace(/\s/g, '');
+
+  const guid = withoutEscapeCharacters(`
     ${S4().repeat(2)}
     ${`-${S4()}`.repeat(4)}
     ${S4().repeat(2)}
-  `
-    .replace(/\r?\n|\r/g, ' ')
-    .replace(/\s/g, '');
+  `);
 
   return guid;
 };
