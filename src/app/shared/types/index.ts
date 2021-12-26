e6a1625a77317cacc7ce950db83580ac30c0ba98
funny family <maksvadik228@gmail.com>
2021-12-26 17:57:48 +0700

    update global types

--- src/app/shared/types/index.ts
+++ src/app/shared/types/index.ts
@@ -90,3 +90,11 @@ export type Writeable<T extends { [x: string]: any }, K extends string> = {
 
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export type SingleProperty<T, K extends keyof T> = K extends any
+  ? { [Prop in K]: T[Prop] }
+  : never;
+
+export type UnionOfProperties<T> = {
+  [K in keyof T]: SingleProperty<T, K>;
+}[keyof T];
