42191d51b9b806d563a3744c6f3cedf71e82dbcc
funny family <maksvadik228@gmail.com>
2022-01-16 18:38:25 +0700

    update "v-model-modifier" util

--- src/app/shared/utils/v-model-modifier.ts
+++ src/app/shared/utils/v-model-modifier.ts
@@ -1,10 +1,28 @@
-interface VModelModifierInterface {}
+import type { Prop, PropType } from 'vue';
 
-interface VModelModifierConstructor {}
+type Name = 'modelModifiers';
 
-export class VModelModifier implements VModelModifierInterface {
-  // eslint-disable-next-line
-  constructor({}: VModelModifierConstructor) {
-    //
+type Modifier<T extends string> = Record<T, boolean>;
+
+interface VModelModifierFields<M extends string> {
+  propName: Name;
+  propObject: Prop<Modifier<M>>;
+}
+
+export class VModelModifier<M extends string>
+  implements VModelModifierFields<M>
+{
+  private readonly name: Name = 'modelModifiers';
+
+  get propName(): Name {
+    return this.name;
+  }
+
+  get propObject(): Prop<Modifier<M>> {
+    return {
+      type: Object as PropType<Modifier<M>>,
+      required: false,
+      default: () => ({})
+    };
   }
 }
