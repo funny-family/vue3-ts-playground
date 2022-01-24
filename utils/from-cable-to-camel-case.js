a92aeee388883fb2d081d50c3c1a16532db905e7
funny family <maksvadik228@gmail.com>
2022-01-24 20:31:48 +0700

    update utils for development

---
+++ utils/from-cable-to-camel-case.js
@@ -0,0 +1,15 @@
+const fromCableToCamelCase = (str) => {
+  if (str === undefined || str === null) return null;
+
+  const arr = str.split('-');
+  const capital = arr.map((item, index) =>
+    index
+      ? `${item.charAt(0).toUpperCase()}${item.slice(1).toLowerCase()}`
+      : item.toLowerCase()
+  );
+  const capitalString = capital.join('');
+
+  return capitalString;
+};
+
+exports.fromCableToCamelCase = fromCableToCamelCase;
