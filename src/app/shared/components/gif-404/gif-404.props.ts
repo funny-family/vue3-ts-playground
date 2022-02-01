5df8d4777907d7f09a567b7e4b404825a503c59b
funny-family <maksvadik228@gmail.com>
2022-02-01 23:32:40 +0700

    update "gif-404" prop file

--- src/app/shared/components/gif-404/gif-404.props.ts
+++ src/app/shared/components/gif-404/gif-404.props.ts
@@ -1,102 +1,31 @@
 import type { ExtractPropTypes } from '@vue/runtime-core';
+import type { IfAny } from '@vue/shared';
 import type { HTMLAttributes, Prop, PropType } from 'vue';
 import type { RecordPropsDefinition } from '../../types';
-
-// type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N;
-
-// type InferPropType<T> = [T] extends [null]
-//   ? any
-//   : [T] extends [
-//       {
-//         type: null | true;
-//       }
-//     ]
-//   ? any
-//   : [T] extends [
-//       | ObjectConstructor
-//       | {
-//           type: ObjectConstructor;
-//         }
-//     ]
-//   ? Record<string, any>
-//   : [T] extends [
-//       | BooleanConstructor
-//       | {
-//           type: BooleanConstructor;
-//         }
-//     ]
-//   ? boolean
-//   : [T] extends [
-//       | DateConstructor
-//       | {
-//           type: DateConstructor;
-//         }
-//     ]
-//   ? Date
-//   : [T] extends [
-//       | (infer U)[]
-//       | {
-//           type: (infer U)[];
-//         }
-//     ]
-//   ? U extends DateConstructor
-//     ? Date | InferPropType<U>
-//     : InferPropType<U>
-//   : [T] extends [Prop<infer V, infer D>]
-//   ? unknown extends V
-//     ? IfAny<V, V, D>
-//     : V
-//   : T;
-
-// type RequiredKeys<T> = {
-//   [K in keyof T]: T[K] extends
-//     | {
-//         required: true;
-//       }
-//     | {
-//         default: any;
-//       }
-//     | BooleanConstructor
-//     | {
-//         type: BooleanConstructor;
-//       }
-//     ? T[K] extends {
-//         default: undefined | (() => undefined);
-//       }
-//       ? never
-//       : K
-//     : never;
-// }[keyof T];
-
-// type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>;
-
-// export declare type ExtractPropTypes1<O> = O extends object
-//   ? {
-//       [K in RequiredKeys<O>]: InferPropType<O[K]>;
-//     } &
-//       {
-//         [K in keyof O]?: InferPropType<O[K]>;
-//       }
-//   : {
-//       [K in string]: any;
-//     };
-
-// export declare type ExtractPropTypes1<O> = {
-//   [K in keyof Pick<O, RequiredKeys<O>>]: InferPropType<O[K]>;
-// } | {
-//   [K in keyof Pick<O, OptionalKeys<O>>]: InferPropType<O[K]>;
-// };
-
-// type PropSchema<DefaultValue, RequiredValue, TypeValue, ValidatorFunction = (value: unknown) => boolean> = {};
 
 type Data = Record<string, unknown>;
 
 type DefaultFactory<T> = (props: Data) => T | null | undefined;
 
+// type PropMethod<T, TConstructor = any> = [T] extends [
+//   ((...args: any) => any) | undefined
+//   ] ? {
+//       new (): TConstructor;
+//       (): T;
+//       readonly prototype: TConstructor;
+//   } : never;
+
+// type PropConstructor<T = any> = {
+//   new (...args: any[]): T & {};
+// } | {
+//   (): T;
+// } | PropMethod<T>;
+
+
 type PropOption<
-  TypeValue,
+  TypeValue extends any,
   RequiredValue extends boolean,
-  DefaultValue,
+  DefaultValue extends TypeValue,
   ValidatorFunction = (value: unknown) => boolean
 > = {
   type: PropType<TypeValue> | true | null;
@@ -105,19 +34,19 @@ type PropOption<
   validator?: ValidatorFunction;
 };
 
-interface PropSchema {
-  icon: PropOption<String, false, ''>;
-  title: PropOption<String, true, ''>;
+interface Gif404PropSchema {
+  icon: PropOption<string, false, ''>;
+  text: PropOption<string, true, ''>;
 }
 
-const p: PropSchema = {
+const p: Gif404PropSchema = {
   icon: {
     default: () => '11111111',
     required: false,
     type: String
   },
 
-  title: {
+  text: {
     default: '',
     required: true,
     type: String
@@ -149,42 +78,46 @@ export const props: RecordPropsDefinition<Gif404PropFields> = {
 
 //  =================================================================
 
-// type RequiredKeys<T> = {
-//   [K in keyof T]-?: {} extends Pick<T, K> ? never : K;
-// }[keyof T];
-// type OptionalKeys<T> = {
-//   [K in keyof T]-?: {} extends Pick<T, K> ? K : never;
-// }[keyof T];
+type InferPropType<T> = [T] extends [null] ? any : [T] extends [{
+  type: null | true;
+}] ? any : [T] extends [ObjectConstructor | {
+  type: ObjectConstructor;
+}] ? Record<string, any> : [T] extends [BooleanConstructor | {
+  type: BooleanConstructor;
+}] ? boolean : [T] extends [DateConstructor | {
+  type: DateConstructor;
+}] ? Date : [T] extends [(infer U)[] | {
+  type: (infer U)[];
+}] ? U extends DateConstructor ? Date | InferPropType<U> : InferPropType<U> : [T] extends [Prop<infer V, infer D>] ? unknown extends V ? IfAny<V, V, D> : V : T;
 
 //  =================================================================
 
-// not cool!
-type Prp = typeof props;
 
-type ReqKey<T> = {
+type PropRequireKey<T> = {
   [K in keyof T]: T[K] extends { required: true } ? K : never;
 }[keyof T];
 
-// type OptKey<T> = {
-//   [K in keyof T]: T[K] extends { required: false } ? K : never;
-// }[keyof T];
+type PropOptionalKey<T> = Exclude<keyof T, PropRequireKey<T>>;
 
-type OptKey<T> = Exclude<keyof T, ReqKey<T>>;
+// type ExtractTypeFromProps<T> = {
+//   [K in PropRequireKey<T>]: InferPropType<T[K]>;
+// } &
+//   {
+//     [K in PropOptionalKey<T>]?: InferPropType<T[K]>;
+//   };
 
-type PropTypeField<T> = {
-  // [K in keyof T]: T['type'];
-};
+type ExtractTypeFromProps<O> = O extends object
+  ? { [K in keyof O]?: unknown } & // This is needed to keep the relation between the option prop and the props, allowing to use ctrl+click to navigate to the prop options. see: #3656
+      { [K in PropRequireKey<O>]: InferPropType<O[K]> } &
+      { [K in PropOptionalKey<O>]?: InferPropType<O[K]> }
+  : O extends undefined
+  ? {}
+  : { [K in string]: any }
 
-type ExtractTypeFromProps<T> = {
-  [K in ReqKey<T>]: unknown;
-} &
-  {
-    [K in OptKey<T>]?: unknown;
-  };
+// YEAH!!! DAT SHIIIT WOORK!
+type TheRealPropType = ExtractTypeFromProps<Gif404PropSchema>;
 
-type P = ExtractTypeFromProps<PropSchema>;
-
-export type Gif404Props = ExtractPropTypes<typeof props>;
+export type Gif404Props = ExtractTypeFromProps<Gif404PropSchema>;
 // export type Gif404Props = ExtractPropTypes<typeof props> & ThisType<void>;
 // export type Gif404Props = ExtractPropTypes1<typeof props>;
 
