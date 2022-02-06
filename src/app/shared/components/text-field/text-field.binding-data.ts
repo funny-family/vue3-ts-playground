bf652a7689b16e56a37ee6e2cedc9a9613081266
funny family <maksvadik228@gmail.com>
2022-02-06 13:18:33 +0700

    update "binding-data" file in "text-field" component

--- src/app/shared/components/text-field/text-field.binding-data.ts
+++ src/app/shared/components/text-field/text-field.binding-data.ts
@@ -2,8 +2,10 @@ import {
   VModelArgument,
   defaultVModelArgumentName
 } from '@/app/shared/utils/v-model-argument';
+import type { RecordOfBoolean } from '@/app/shared/types';
 import { VModelModifier } from '@/app/shared/utils/v-model-modifier';
 import type { DefaultVModelArgumentName } from '@/app/shared/utils/v-model-argument';
+import type { PropOption as _PropOption } from '@/app/shared/types/component/props';
 import type { VModel as _VModel } from '@/app/shared/types/directives';
 
 export namespace VModel {
@@ -11,7 +13,8 @@ export namespace VModel {
 
   export namespace Argument {
     export namespace ModelValue {
-      export type Type = string;
+      type Type = string;
+      export type PropOption = _PropOption<Type, false, Type>;
 
       export const { propName, propObject, nameOfEmit, normalizedNameOfEmit } =
         new VModelArgument<DefaultVModelArgumentName, Type>({
@@ -27,14 +30,16 @@ export namespace VModel {
 
   export namespace ModelModifier {
     // list of modifiers
-    export type Modifier = Argument.ModelValue.Modifier.Type;
+    export type ModifierList = Argument.ModelValue.Modifier.Type;
+    type ModifierRecord = RecordOfBoolean<ModifierList>;
+    export type PropOption = _PropOption<ModifierRecord, false, ModifierRecord>;
 
-    export const { propName, propObject } = new VModelModifier<Modifier>();
+    export const { propName, propObject } = new VModelModifier<ModifierList>();
   }
 
   export type Directive = _VModel.Directive<
     Type,
     string,
-    ModelModifier.Modifier
+    ModelModifier.ModifierList
   >;
 }
