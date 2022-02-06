4ccd26bbfb1c6e14656631ce81e5177d01de874a
funny family <maksvadik228@gmail.com>
2022-02-06 13:33:45 +0700

    remove "jss" stuff

--- src/app/shared/components/header/header.component.tsx
+++ src/app/shared/components/header/header.component.tsx
@@ -1,23 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { defineComponent, getCurrentInstance, withScopeId } from 'vue';
 
 // import { setup } from './header.setup';
 // import { props } from './header.props';
-
-import jss from 'jss';
 /**
  * @see https://github.com/BestVue3/vue-jss
  * @see https://python.iitter.com/other/264524.html
  */
-const s = jss
-  .createStyleSheet({
-    app: {
-      backgroundColor: 'green',
-      color: 'yellow'
-    }
-  })
-  .attach();
-console.log(1123213, s.classes.app);
 
 export const Header = defineComponent({
   name: 'Header',
@@ -40,7 +28,7 @@ export const Header = defineComponent({
   render() {
     return (
       <header style={{ height: '60px', backgroundColor: 'red' }}>
-        <div class={s.classes.app}>adadad34</div>
+        <div>adadad34</div>
         {this.$props.title}
       </header>
     );
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './jss.config';
 import { createApp } from 'vue';
 import App from './app/App.vue';
 import { router } from './app/router';
