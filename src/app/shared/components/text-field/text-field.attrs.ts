7fdde3984d7d7898aeaeb62c38b9674fb7a8bc6a
funny family <maksvadik228@gmail.com>
2022-02-21 22:46:11 +0700

    update "attr" types of "text-field" component

--- src/app/shared/components/text-field/text-field.attrs.ts
+++ src/app/shared/components/text-field/text-field.attrs.ts
@@ -1,5 +1,9 @@
 import type { InputHTMLAttributes } from 'vue';
-import type { HTMLAttributesWithClassAttributeAsString } from '@/app/shared/types/component/attrs';
+import type {
+  HTMLAttributesWithClassAttributeAsString,
+  WithoutVModelAttr
+} from '@/app/shared/types/component/attrs';
 
-export type TextFieldAttrs =
-  HTMLAttributesWithClassAttributeAsString<InputHTMLAttributes>;
+export type TextFieldAttrs = WithoutVModelAttr<
+  HTMLAttributesWithClassAttributeAsString<InputHTMLAttributes>
+>;
