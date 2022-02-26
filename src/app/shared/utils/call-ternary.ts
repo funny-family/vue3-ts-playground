fe4daee5863578be9cf561ea3df29f7d9de12fda
funny family <maksvadik228@gmail.com>
2022-02-26 18:22:15 +0700

    update api of "call-ternary" util

--- src/app/shared/utils/call-ternary.ts
+++ src/app/shared/utils/call-ternary.ts
@@ -1,11 +1,24 @@
 interface TernaryObject<I, E> {
   condition: boolean;
-  onIf: () => I;
-  onElse: () => E;
+  onTruthy : () => I | void;
+  onFalsy: () => E | void;
 }
 
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+ *
+ * @description
+ * Functional abstraction for ternary operator.
+ *
+ * @example
+ * callTernary({
+ *    condition: this.counter % 2 === 0,
+ *    onTruthy: () => 'even number!',
+ *    onFalsy: () => 'odd number!'
+ * })
+ */
 export const callTernary = <I, E>({
   condition,
-  onIf,
-  onElse
-}: TernaryObject<I, E>) => (condition ? onIf() : onElse());
+  onTruthy,
+  onFalsy
+}: TernaryObject<I, E>) => (condition ? onTruthy() : onFalsy());
