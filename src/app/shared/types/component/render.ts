8884ce1fcc347673dac5b98aeca6a3867443f5f0
funny family <maksvadik228@gmail.com>
2022-03-01 00:04:07 +0700

    update "render" type

--- src/app/shared/types/component/render.ts
+++ src/app/shared/types/component/render.ts
@@ -1,8 +1,24 @@
-export type RenderFunction<T> = (
-  this: T,
-  ctx,
-  cache,
-  $props,
-  $setup,
-  $options
-) => {};
+import type { VNode, VNodeTypes } from 'vue';
+import type { Data } from '@/app/shared/types';
+
+type D = Record<string, undefined>;
+
+type RenderFunctionReturn = VNodeTypes | JSX.Element;
+
+export type RenderFunction<
+  This,
+  Ctx = D,
+  Cache = VNode[],
+  $Props = D,
+  $Setup = D,
+  $Data = D,
+  $Options = D
+> = (
+  this: This,
+  ctx: Ctx,
+  cache: Cache,
+  $props: $Props,
+  $setup: $Setup,
+  $data: $Data,
+  $options: $Options
+) => RenderFunctionReturn;
