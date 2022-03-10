610f06c5dfb7c60bd570268bb29ed1859a7328e5
funny family <maksvadik228@gmail.com>
2022-03-11 00:11:07 +0700

    extand type for "vue"

--- src/@types/vue/index.ts
+++ src/@types/vue/index.ts
@@ -1,3 +1,11 @@
+import type { VModel } from '@/app/shared/types/directives';
+
 export * from '../../../node_modules/vue/dist/vue';
 
 export { Transition } from './transition';
+export { Suspense } from './suspense';
+
+declare module 'vue' {
+  export interface InputHTMLAttributes extends VModel.Directive {}
+  export interface SelectHTMLAttributes extends VModel.Directive {}
+}
