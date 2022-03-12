8be2cc32fc52ca2717f9886a0bfad963a63880c2
funny family <maksvadik228@gmail.com>
2022-03-13 00:02:20 +0700

    update type for "vue"

--- src/@types/vue/index.ts
+++ src/@types/vue/index.ts
@@ -1,4 +1,4 @@
-import type { VModel } from '@/app/shared/types/directives';
+import type { VShow, VModel } from '@/app/shared/types/directives';
 
 export * from '../../../node_modules/vue/dist/vue';
 
@@ -6,6 +6,9 @@ export { Transition } from './transition';
 export { Suspense } from './suspense';
 
 declare module 'vue' {
+  export interface HTMLAttributes extends VShow.Directive {}
+
   export interface InputHTMLAttributes extends VModel.Directive {}
+  export interface TextareaHTMLAttributes extends VModel.Directive {}
   export interface SelectHTMLAttributes extends VModel.Directive {}
 }
