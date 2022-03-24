d18db38d4fd4dd271a7b207da65977fc95a0ec8a
funny family <maksvadik228@gmail.com>
2022-03-24 22:30:39 +0700

    update "text-field" component

--- src/app/shared/components/text-field/text-field.component.ts
+++ src/app/shared/components/text-field/text-field.component.ts
@@ -3,16 +3,16 @@ import { setup, TextFieldBindings } from './text-field.setup';
 import { props, TextFieldProps } from './text-field.props';
 import { emits, TextFieldEmits } from './text-field.emits';
 import { render } from './text-field.render';
-import { nameOf } from '../../utils/name-of';
 import type { VModel } from './text-field.binding-data';
 import type { TextFieldAttrs } from './text-field.attrs';
+import { nameOf } from '@/app/shared/utils/name-of';
 
 export const TextField = defineComponent<
   TextFieldProps & TextFieldAttrs & TextFieldEmits.AsProps & VModel.Directive,
   TextFieldBindings
 >({});
 
-// @ts-ignore
+// @ts-expect-error
 TextField.name = nameOf(() => TextField);
 TextField.inheritAttrs = false;
 TextField.setup = setup;
