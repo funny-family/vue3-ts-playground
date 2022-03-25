2eddc6ad961bdccc313171879e131f74cabbb79b
funny-family <maksvadik228@gmail.com>
2022-03-25 23:42:52 +0700

    add "is-upper-cased" util

---
+++ src/app/shared/utils/is-upper-cased.ts
@@ -0,0 +1,14 @@
+// https://iqcode.com/code/javascript/javascript-check-if-string-contains-capital-letter
+
+// /**
+//  * @description
+//  * adada
+//  *
+//  * @example
+//  * adada
+//  */
+// export const isUpperCased = (string: string): boolean => {
+//   return !/[a-z]/.test(string) && /[A-Z]/.test(string);
+// };
+
+
