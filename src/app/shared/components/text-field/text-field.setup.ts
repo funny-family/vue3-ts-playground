6114fc2c1ea7aac3ee15fc1804ced271a15a7bee
funny-family <maksvadik228@gmail.com>
2022-03-25 23:43:41 +0700

    update "text-field" component

--- src/app/shared/components/text-field/text-field.setup.ts
+++ src/app/shared/components/text-field/text-field.setup.ts
@@ -1,21 +1,22 @@
-import type { EmitsOptions, SetupContext } from 'vue';
 import type { SetupCtx } from '@/app/shared/types/component/setup';
 import type { TextFieldProps } from './text-field.props';
 import type { TextFieldAttrs } from './text-field.attrs';
 import { TextFieldEmits, emitName } from './text-field.emits';
+import { capitalize } from '@/app/shared/utils/capitalize';
+import { isCapitalized } from '@/app/shared/utils/is-capitalized';
 
-export const setup = (p: Readonly<{}>, ctx: SetupContext<EmitsOptions>) => {
-  const props = p as Readonly<TextFieldProps>;
-  const context = ctx as unknown as SetupCtx<
-    TextFieldEmits.AsFunctionArguments,
-    TextFieldAttrs
-  >;
+type Props = Readonly<TextFieldProps>;
+type Context = SetupCtx<TextFieldEmits.AsFunctionArguments, TextFieldAttrs>;
 
+export const setup = (props: Props, context: Context) => {
   const onInput = (event: Event): void => {
     let value = (event.target as HTMLInputElement).value;
 
-    if (props.modelModifiers?.capitalize) {
-      value = value.charAt(0).toUpperCase() + value.slice(1);
+    if (
+      props.modelModifiers?.capitalize === true &&
+      isCapitalized(value) === false
+    ) {
+      value = capitalize(value);
     }
 
     context.emit(emitName.updateModelValue, value);
