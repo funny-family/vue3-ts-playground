7b27548d1f1927a1fd242a2d0e3b4c16258f1ab1
funny family <maksvadik228@gmail.com>
2022-03-27 14:08:07 +0700

    update "is-upper-cased" util

--- src/app/shared/utils/is-upper-cased.ts
+++ src/app/shared/utils/is-upper-cased.ts
@@ -1,14 +1,13 @@
-// https://iqcode.com/code/javascript/javascript-check-if-string-contains-capital-letter
-
-// /**
-//  * @description
-//  * adada
-//  *
-//  * @example
-//  * adada
-//  */
-// export const isUpperCased = (string: string): boolean => {
-//   return !/[a-z]/.test(string) && /[A-Z]/.test(string);
-// };
-
-
+/**
+ * @see https://iqcode.com/code/javascript/javascript-check-if-string-contains-capital-letter
+ *
+ * @description
+ * Check if all string characters is in uppercase.
+ *
+ * @example
+ * const upperCasedString = 'HELLO!';
+ * const isStringUpperCased = isUpperCased(upperCasedString);
+ * console.log(isStringUpperCased); // true
+ */
+export const isUpperCased = (string: string): boolean =>
+  /^\b[A-Z]*\b/g.test(string);
