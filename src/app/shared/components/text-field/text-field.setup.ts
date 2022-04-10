1f8e1e2a222b477d21fb781e74a08251a54d0943
funny family <maksvadik228@gmail.com>
2022-04-10 22:51:05 +0700

    update type of "setup" function of "text-field" component

--- src/app/shared/components/text-field/text-field.setup.ts
+++ src/app/shared/components/text-field/text-field.setup.ts
@@ -1,12 +1,19 @@
-import type { SetupCtx } from '@/app/shared/types/component/setup';
 import type { TextFieldProps } from './text-field.props';
 import type { TextFieldAttrs } from './text-field.attrs';
-import { TextFieldEmits, emitName } from './text-field.emits';
+import type { TextFieldEmits } from './text-field.emits';
+import type {
+  ContextOfSetupFunction,
+  PropsOfSetupFunction
+} from '@/app/shared/types/component/setup';
+import { emitName } from './text-field.emits';
 import { capitalize } from '@/app/shared/utils/capitalize';
 import { isCapitalized } from '@/app/shared/utils/is-capitalized';
 
-type Props = Readonly<TextFieldProps>;
-type Context = SetupCtx<TextFieldEmits.AsFunctionArguments, TextFieldAttrs>;
+type Props = PropsOfSetupFunction<TextFieldProps>;
+type Context = ContextOfSetupFunction<
+  TextFieldEmits.AsFunctionArguments,
+  TextFieldAttrs
+>;
 
 export const setup = (props: Props, context: Context) => {
   const onInput = (event: Event): void => {
