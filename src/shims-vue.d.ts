9469dcfa891ec85bc360b2d7595f5e5b4022017c
funny-family <maksvadik228@gmail.com>
2022-04-23 13:01:20 +0700

    update "shims" types

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -14,3 +14,8 @@ declare module '*.scss' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '*.svg' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
