fd6430aced0d6854a2744b61cbbbacfb4604b186
funny-family <maksvadik228@gmail.com>
2022-05-18 21:45:14 +0700

    update "text-field" component

--- src/app/shared/components/text-field/text-field.component.ts
+++ src/app/shared/components/text-field/text-field.component.ts
@@ -1,10 +1,13 @@
-import { defineComponent } from 'vue';
-import { setup, TextFieldBindings } from './text-field.setup';
-import { props, TextFieldProps } from './text-field.props';
-import { emits, TextFieldEmits } from './text-field.emits';
-import { render } from './text-field.render';
+import type { TextFieldProps } from './text-field.props';
+import type { TextFieldBindings } from './text-field.setup';
+import type { TextFieldEmits } from './text-field.emits';
 import type { VModel } from './text-field.binding-data';
 import type { TextFieldAttrs } from './text-field.attrs';
+import { defineComponent } from 'vue';
+import { setup } from './text-field.setup';
+import { props } from './text-field.props';
+import { emits } from './text-field.emits';
+import { render } from './text-field.render';
 import { nameOf } from '@/app/shared/utils/name-of';
 
 type Props = TextFieldProps &
