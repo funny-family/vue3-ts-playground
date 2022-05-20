12cf0a18a06d624be9e63d54f0484994242f1fa4
funny-family <maksvadik228@gmail.com>
2022-05-20 23:51:15 +0700

    add shared type file for component

---
+++ src/app/shared/types/component/index.ts
@@ -0,0 +1,56 @@
+import type { ComponentPublicInstance, ObjectEmitsOptions, Slots } from 'vue';
+import type { Data } from '@/app/shared/types';
+import { EmitFunction } from './emits';
+
+export type PublicInstanceOfComponent = ComponentPublicInstance;
+
+// $: ComponentInternalInstance;
+//     $data: D;
+//     $props: MakeDefaultsOptional extends true ? Partial<Defaults> & Omit<P & PublicProps, keyof Defaults> : P & PublicProps;
+//     $attrs: Data;
+//     $refs: Data;
+//     $slots: Slots;
+//     $root: ComponentPublicInstance | null;
+//     $parent: ComponentPublicInstance | null;
+//     $emit: EmitFn<E>;
+//     $el: any;
+//     $options: Options & MergedComponentOptionsOverride;
+//     $forceUpdate: () => void;
+//     $nextTick: typeof nextTick;
+
+/**
+ * $props: MakeDefaultsOptional extends true ? Partial<Defaults> & Omit<P & PublicProps, keyof Defaults> : P & PublicProps;
+ */
+export type PropsOfComponentPublicInstance<T = Data> = {
+  $attrs: T;
+};
+
+/**
+ * $attrs: Data;
+ */
+export type AttrsOfComponentPublicInstance<T = Data> = {
+  $attrs: T;
+};
+
+/**
+ * $slots: Slots;
+ */
+export type SlotsOfComponentPublicInstance<T = Slots> = {
+  $slots: T;
+};
+
+/**
+ * $emit: EmitFn<E>;
+ */
+export type EmitOfComponentPublicInstance<
+  T = EmitFunction<ObjectEmitsOptions>
+> = {
+  $emit: T;
+};
+
+/**
+ * $el: any;
+ */
+export type ELOfComponentPublicInstance<T = HTMLElement> = {
+  $el: T;
+};
