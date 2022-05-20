7fdf66a9ef5be579e1ff694a2c9a0260696900c2
funny-family <maksvadik228@gmail.com>
2022-05-21 00:02:55 +0700

    update prettier related files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
+/**
+ * @typedef { import("prettier").RequiredOptions } RequiredOption
+ */
+
+/** @type {RequiredOption} */
 module.exports = {
   jsxBracketSameLine: false,
+  bracketSameLine: false,
   htmlWhitespaceSensitivity: 'css',
   singleQuote: true,
   trailingComma: 'none',
-  printWidth: 80
+  printWidth: 80,
+  endOfLine: 'lf',
+  singleAttributePerLine: true
 };
