6f95f796ef4359654c59d8c61266557de5124af7
funny-family <maksvadik228@gmail.com>
2022-05-25 22:15:18 +0700

    add "merge-strings" util

---
+++ src/app/shared/utils/merge-strings.ts
@@ -0,0 +1,6 @@
+import clsx from 'clsx';
+import type { ClassArray, ClassDictionary, ClassValue } from 'clsx';
+
+const mergeStrings = clsx;
+
+export { mergeStrings, ClassArray, ClassDictionary, ClassValue };
