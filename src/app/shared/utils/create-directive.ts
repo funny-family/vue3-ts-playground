09fbc0340cb6fbdd4b2c7a897ad2fb30e0c306fb
funny-family <maksvadik228@gmail.com>
2022-05-25 22:10:51 +0700

    update "create-directive" util

--- src/app/shared/utils/create-directive.ts
+++ src/app/shared/utils/create-directive.ts
@@ -1,31 +1,83 @@
-import type { VNode, ObjectDirective, Directive, DirectiveArguments } from 'vue';
+import type {
+  VNode,
+  ObjectDirective,
+  Directive,
+  DirectiveArguments
+} from 'vue';
 import type { RequireAtLeastOne } from '@/app/shared/types';
-// import type { DirectiveRegisterFunction } from '@/app/shared/types/component/directive';
+import type {
+  DirectiveModifier,
+  DirectiveOption
+} from '@/app/shared/types/component/directive';
 
-// /*
-//   V = value/oldValue, A = arg, M = modifiers, D = any
-// */
-// export interface _DirectiveBinding<
-//   V = any,
-//   A = string,
-//   M = DirectiveModifier<string>,
-//   D = any
-// > {
-//   instance: ComponentPublicInstance<Data, Data, Data> | null;
-//   value: V;
-//   oldValue: V | null;
-//   arg?: A;
-//   modifiers: M;
-//   dir: ObjectDirective<D, V>;
-// }
+// =================================================================
+// type D<V, A, M> = V extends void
+//   ? {
+//       arg: A;
+//       mod: M;
+//     }
+//   : A extends void
+//   ? {
+//       val: A;
+//       mod: M;
+//     }
+//   : M extends void
+//   ? {
+//       val: A;
+//       arg: A;
+//     }
+//   : {
+//     val: A;
+//     arg: A;
+//     mod: M;
+//   };
+
+// const d: D<void, '1' | '2' | '3', 'one' | 'two' | 'three'> = {
+
+// };
+// =================================================================
 
 type DirectiveRegisterFunction = () => Record<string, Directive>;
 
-type DirectiveUseFunction = (directiveArgument?: {
-  value?: any;
-  argument?: string;
-  modifiers?: Record<string, unknown>;
-}) => DirectiveArguments;
+// type DirectiveUseFunctionArgument<V = any, A = string, M = {}> = V extends void
+//   ? {
+//       arg: A;
+//       modifiers: M;
+//     }
+//   : A extends void
+//   ? {
+//       value: A;
+//       modifiers: M;
+//     }
+//   : M extends void
+//   ? {
+//       val: A;
+//       arg: A;
+//     }
+//   : {
+//       value: A;
+//       arg: A;
+//       modifiers: M;
+//     };
+
+// type R = DirectiveUseFunctionArgument<any, string, {}>;
+
+// type DirectiveUseFunction<V = any, A = string, M extends string = string> = (
+//   directiveArgument?: Partial<{
+//     value: V;
+//     arg: A;
+//     modifiers: DirectiveModifier<M>;
+//   }>
+// ) => DirectiveArguments;
+
+type DirectiveUseFunction<V = any, A = string, M extends any = string> = (
+  directiveArgument?: Partial<{
+    value: V;
+    arg: A;
+    // @ts-expect-error <-- I gave up here ;(
+    modifiers: M extends void ? void : DirectiveModifier<M>;
+  }>
+) => DirectiveArguments;
 
 /**
  * @see https://vuejs.org/guide/reusability/custom-directives.html#introduction
@@ -34,31 +86,84 @@ type DirectiveUseFunction = (directiveArgument?: {
  * adada
  *
  * @example
- * adad
+ * type VFontDirectiveModifier = 'normal' | 'italic' | 'oblique';
+ *
+ * export const vFontDirective = createDirective<
+ *  HTMLElement,
+ *  number,
+ *  void,
+ *  VFontDirectiveModifier
+ * >('font', {
+ *   beforeMount(el, binding) {
+ *     if (binding.modifiers.normal === true) {
+ *       el.style.fontStyle = 'normal';
+ *     } else if (binding.modifiers.italic === true) {
+ *       el.style.fontStyle = 'italic';
+ *     } else if (binding.modifiers.oblique === true) {
+ *       el.style.fontStyle = 'oblique';
+ *     }
+ *
+ *     el.style.fontSize = `${binding.value}px`;
+ *   },
+ *
+ *   updated(el, binding) {
+ *     el.style.fontSize = `${binding.value}px`;
+ *   },
+ * });
+ *
+ * ...
+ *
+ * SomeComponent.directives = {
+ *   ...vFontDirective.register()
+ * };
+ *
+ * ...
+ *  {withDirectives(<h1>Heading</h1>, [
+ *     vFontDirective.use({
+ *       value: 40,
+ *       modifiers: {
+ *         italic: true
+ *       }
+ *     })
+ *   ])}
+ * ...
  */
 export const createDirective = <
-  ObjectDirectiveTypeSchema extends ObjectDirective
+  DirectiveElement extends HTMLElement = HTMLElement,
+  DirectiveValue = any,
+  DirectiveArg = string,
+  DirectiveModifier extends any = string
 >(
   name: Readonly<string>,
-  directiveObject: RequireAtLeastOne<ObjectDirectiveTypeSchema>
+  directiveObject: RequireAtLeastOne<
+    DirectiveOption<
+      DirectiveElement,
+      DirectiveValue,
+      DirectiveArg,
+      // @ts-expect-error <-- And here too ;(
+      DirectiveModifier
+    >
+  >
 ) => {
-  const register: DirectiveRegisterFunction = () => ({
-    [name]: directiveObject
-  });
+  const register: DirectiveRegisterFunction = () =>
+    ({
+      [name]: directiveObject
+    } as any);
 
-  const use: DirectiveUseFunction = (directiveArgument) => [
-    directiveObject,
-    directiveArgument?.value,
-    directiveArgument?.argument,
-    directiveArgument?.modifiers
-  ];
+  const use: DirectiveUseFunction<
+    DirectiveValue,
+    DirectiveArg,
+    DirectiveModifier
+  > = (directiveArgument) =>
+    [
+      directiveObject,
+      directiveArgument?.value,
+      directiveArgument?.arg,
+      directiveArgument?.modifiers
+    ] as any[];
 
   return {
     register,
     use
   };
 };
-
-const vSomething = createDirective('something', {
-  deep: false
-});
