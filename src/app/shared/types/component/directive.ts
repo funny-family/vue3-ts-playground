769293becac3f811b7ce194ab1b03174a5a49e9d
funny-family <maksvadik228@gmail.com>
2022-05-25 22:08:25 +0700

    update "directive" shared types

--- src/app/shared/types/component/directive.ts
+++ src/app/shared/types/component/directive.ts
@@ -6,6 +6,41 @@ import type {
 } from 'vue';
 import type { Data } from '@/app/shared/types';
 
+/**
+ * M = Modifiers
+ */
+export type DirectiveModifier<M extends string> = Partial<
+  Record<M, true | undefined>
+>;
+
+/*
+  V = value/oldValue, A = arg, M = modifiers, D = any
+
+  export declare interface DirectiveBinding<V = any> {
+    instance: ComponentPublicInstance | null;
+    value: V;
+    oldValue: V | null;
+    arg?: string;
+    modifiers: DirectiveModifiers;
+    dir: ObjectDirective<any, V>;
+  }
+*/
+export interface _DirectiveBinding<
+  V = any,
+  A = string,
+  M extends string = string,
+  D = any
+> {
+  instance: ComponentPublicInstance | null;
+  value: V;
+  oldValue: V | null;
+  arg?: A;
+  modifiers: DirectiveModifier<M>;
+  dir: ObjectDirective<D, V>;
+}
+
+// export declare type DirectiveHook<T = any, Prev = VNode<any, T> | null, V = any> = (el: T, binding: DirectiveBinding<V>, vnode: VNode<any, T>, prevVNode: Prev) => void;
+
 // export declare interface ObjectDirective<T = any, V = any> {
 //   created?: DirectiveHook<T, null, V>;
 //   beforeMount?: DirectiveHook<T, null, V>;
@@ -18,84 +53,29 @@ import type { Data } from '@/app/shared/types';
 //   deep?: boolean;
 // }
 
-/**
- * M = Modifiers
- */
-export type DirectiveModifier<M extends string> = Partial<Record<M, true>>;
-
-// /**
-//  * @see https://github.com/vuejs/core/blob/main/packages/runtime-core/src/directives.ts
-//  *
-//  * D = Directive, V = Value, A = Arguments, M = Modifiers
-//  */
-// export type ArgumentsOfDirective<
-//   D = Directive,
-//   V = any,
-//   A = string,
-//   M = DirectiveModifier
-// > = Array<[D] | [D, V] | [D, V, A] | [D, V, A, M]>;
-
-/**
- * D = Directive, V = Value, A = Argument, M = Modifiers
- */
-export type DirectiveArgument<
-  D = Directive,
-  V = any,
-  A = string,
-  M = DirectiveModifier<any>
-> = [D] | [D, V] | [D, V, A] | [D, V, A, M];
-
-/*
-  V = value/oldValue, A = arg, M = modifiers, D = any
-*/
-export interface _DirectiveBinding<
-  V = any,
-  A = string,
-  M = DirectiveModifier<string>,
-  D = any
-> {
-  instance: ComponentPublicInstance<Data, Data, Data> | null;
-  value: V;
-  oldValue: V | null;
-  arg?: A;
-  modifiers: M;
-  dir: ObjectDirective<D, V>;
-}
-
-// export declare type DirectiveHook<T = any, Prev = VNode<any, T> | null, V = any> = (el: T, binding: DirectiveBinding<V>, vnode: VNode<any, T>, prevVNode: Prev) => void;
-
-export type _DirectiveHook<T = any, Prev = VNode<any, T> | null, V = any> = (
-  el: T,
-  binding: _DirectiveBinding<V>,
-  vnode: VNode<any, T>,
-  prevVNode: Prev
-) => void;
-
-export type DirectiveName = {
-  readonly name: string;
-};
-
 export type DirectiveOption<
-  ElArgument extends HTMLElement,
-  BindingArgument extends VNode<any, ElArgument>,
-  VNodeArgument extends VNode<any, ElArgument>
+  DirectiveElement extends HTMLElement,
+  DirectiveValue = any,
+  DirectiveArg = string,
+  DirectiveModifier extends string = string
 > = {
   /**
    * created?: DirectiveHook<T, null, V>;
    */
   created?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    prevVNode: VNode<any, ElArgument> | null
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
+    prevVNode: null
   ) => void;
 
   /**
    * beforeMount?: DirectiveHook<T, null, V>;
    */
   beforeMount?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
     prevVNode: null
   ) => void;
 
@@ -103,9 +83,9 @@ export type DirectiveOption<
    * mounted?: DirectiveHook<T, null, V>;
    */
   mounted?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
     prevVNode: null
   ) => void;
 
@@ -113,29 +93,29 @@ export type DirectiveOption<
    * beforeUpdate?: DirectiveHook<T, VNode<any, T>, V>;
    */
   beforeUpdate?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
-    prevVNode: VNode<any, ElArgument>
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
+    prevVNode: VNode<any, DirectiveElement>
   ) => void;
 
   /**
    * updated?: DirectiveHook<T, VNode<any, T>, V>;
    */
   updated?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
-    prevVNode: VNode<any, ElArgument>
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
+    prevVNode: VNode<any, DirectiveElement>
   ) => void;
 
   /**
    * beforeUnmount?: DirectiveHook<T, null, V>;
    */
   beforeUnmount?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
     prevVNode: null
   ) => void;
 
@@ -143,35 +123,22 @@ export type DirectiveOption<
    * unmounted?: DirectiveHook<T, null, V>;
    */
   unmounted?: (
-    el: ElArgument,
-    binding: BindingArgument,
-    vnode: VNodeArgument,
+    el: DirectiveElement,
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode<any, DirectiveElement>,
     prevVNode: null
   ) => void;
 
   /**
    * getSSRProps?: SSRDirectiveHook;
    */
-  getSSRProps?: (binding: BindingArgument, vnode: VNode) => Data | undefined;
+  getSSRProps?: (
+    binding: _DirectiveBinding<DirectiveValue, DirectiveArg, DirectiveModifier>,
+    vnode: VNode
+  ) => Data | undefined;
 
   /**
    * deep?: boolean;
    */
   deep?: boolean;
-};
-
-export type DirectiveRegisterFunction = {
-  register: () => {
-    /*
-      we have to use "any" ((DirectiveHookOption & DirectiveDeepOption) | any)
-      instead of "DirectiveHookOption & DirectiveDeepOption"
-      otherwise type "directives?: Record<string, Directive>;" from "ComponentOptionsBase" will throw error
-    */
-    [name: string]: any;
-  };
-};
-
-export type DirectiveUseFunction<T = unknown> = {
-  use: (directiveOption?: T) => any[];
-  // use: (directiveOption?: DirectiveOption) => Directive<any, any>;
 };
