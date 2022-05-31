98535c325ec3440fb3be12d864c5bdeac047d08d
funny-family <maksvadik228@gmail.com>
2022-05-31 22:21:11 +0700

    update "with-directives" util

--- src/app/shared/utils/with-directives.ts
+++ src/app/shared/utils/with-directives.ts
@@ -5,12 +5,27 @@ import type { DirectiveArguments, VNode } from 'vue';
  * @see https://vuejs.org/api/render-function.html#withdirectives
  *
  * @description
- * adada
+ * For adding custom directives to vnodes.
  *
  * @example
+ * import { h, withDirectives } from 'vue';
+ *
+ * // a custom directive
+ * const pin = {
+ *   mounted() {
+ *     // ...
+ *   },
+ *   updated() {
+ *     // ...
+ *   },
+ * };
+ *
+ * // <div v-pin:top.animate="200"></div>
+ * const vnode = withDirectives(h('div'), [
+ *   [pin, 200, 'top', { animate: true }]
+ * ])
  */
 export const withDirectives = <T extends VNode>(
   vnode: T,
-  // directives: DirectiveArguments
-  directives: any[]
+  directives: DirectiveArguments
 ) => _withDirectives((openBlock(), vnode), directives);
