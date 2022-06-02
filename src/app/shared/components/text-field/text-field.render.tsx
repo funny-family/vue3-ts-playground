a5cc95829459ef37f5cd2eae5c6a37916d73c38c
funny-family <maksvadik228@gmail.com>
2022-06-02 22:54:10 +0700

    update "text-field" component

--- src/app/shared/components/text-field/text-field.render.tsx
+++ src/app/shared/components/text-field/text-field.render.tsx
@@ -1,21 +1,24 @@
 import type { TextFieldUnwrappedBindings } from './text-field.setup';
 import type { RenderFunction } from '@/app/shared/types/component/render';
-import type { TextFieldInstance } from './text-field.ref';
 import { styles } from './styles/text-field.styles';
+import { mergeStrings } from '@/app/shared/utils/merge-strings';
 
 export const render: RenderFunction<TextFieldUnwrappedBindings> = function () {
   const { props, context, onInput, $, shallowObj, deepObj } = this;
+  const { class: classAttr, style: styleAttr, ...restAttrs } = context.attrs;
   const uid = $.uid;
-  const value = props.modelValue || '';
+  const className = mergeStrings(classAttr, styles.textField);
+  const style = styleAttr;
+  const value = props.modelValue;
 
   return (
-    <div class={styles.textField} data-component-root-el-id={uid}>
-      {props.label !== undefined && (
+    <div class={className} style={style} data-component-root-el-id={uid}>
+      {props.label != null && (
         <label class={styles.textField__label}>{props.label}</label>
       )}
 
       <input
-        {...context.attrs}
+        {...restAttrs}
         class={styles.textField__input}
         type="text"
         value={value}
