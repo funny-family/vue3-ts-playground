a4d45d5e18cf9f4d55b904128f1e61946d4bcaf8
funny-family <maksvadik228@gmail.com>
2022-06-17 23:43:51 +0700

    delete "merge-class-names" util

--- src/app/shared/utils/merge-class-names.ts
+++
@@ -1,6 +0,0 @@
-import clsx from 'clsx';
-import type { ClassArray, ClassDictionary, ClassValue } from 'clsx';
-
-const mergeClassNames = clsx;
-
-export { mergeClassNames, ClassArray, ClassDictionary, ClassValue };
