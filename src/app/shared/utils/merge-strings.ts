28fbcfc7bd03d2933ae07356f2361492d448e853
funny-family <maksvadik228@gmail.com>
2022-06-18 23:44:40 +0700

    delete "merge-strings" util

--- src/app/shared/utils/merge-strings.ts
+++
@@ -1,6 +0,0 @@
-import clsx from 'clsx';
-import type { ClassArray, ClassDictionary, ClassValue } from 'clsx';
-
-const mergeStrings = clsx;
-
-export { mergeStrings, ClassArray, ClassDictionary, ClassValue };
