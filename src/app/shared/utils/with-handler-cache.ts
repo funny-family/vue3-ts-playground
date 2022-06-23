566d4cee5a9096316dde19bc7fb71ac0446dbaa6
funny-family <maksvadik228@gmail.com>
2022-06-23 22:37:06 +0700

    add "with-handler-cache" util

---
+++ src/app/shared/utils/with-handler-cache.ts
@@ -0,0 +1,21 @@
+import type { EventHandler } from '@/app/shared/types';
+
+/**
+ * @see https://dev.to/sharvilak11/vue-3-optimization-via-cached-handlers-2e1j
+ *
+ * @description
+ * adad
+ *
+ * @example
+ * adad
+ */
+export const withHandlerCache = <E extends Event>(
+  handler: (event: E, ...args: any[]) => void,
+  cache: any[],
+  index: number
+): EventHandler<E> =>
+  cache[index] ||
+  (cache[index] = (event: E, ...args: any[]) => handler(event, ...args));
+
+
+// todo: rename function "withHandlerCache" -> "withCachedHandler"
