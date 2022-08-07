f6e2c5f1e11114cd16f76b43d79135f8d6aec31b
funny-family <maksvadik228@gmail.com>
2022-08-07 23:42:41 +0700

    update "with-modifiers" util

--- src/app/shared/utils/with-modifiers.ts
+++ src/app/shared/utils/with-modifiers.ts
@@ -7,12 +7,15 @@ import type {
   RequireOnlyOne,
   UnionOfProperties
 } from '@/app/shared/types';
-import { withModifiers as _withModifiers } from 'vue';
+import { withModifiers as _withModifiers, getCurrentInstance } from 'vue';
 import { createHash } from 'crypto';
 import { capitalize } from '@/app/shared/utils/capitalize';
 import { Modifier } from '@/app/shared/utils/modifiers';
 import { isArrayEmpty } from '@/app/shared/utils/is-array-empty';
 import { callTernary } from '@/app/shared/utils/call-ternary';
+
+// https://vue-next-template-explorer.netlify.app/#eyJzcmMiOiI8aW5wdXQgQGtleXVwLmFsdC5lbnRlci5leGFjdD1cImNsZWFyXCIgLz4iLCJzc3IiOmZhbHNlLCJvcHRpb25zIjp7ImhvaXN0U3RhdGljIjp0cnVlfX0
+// https://stackoverflow.com/questions/2648293/how-to-get-the-function-name-from-within-that-function
 
 type EventObject = {
   [key in keyof Events]: ((event: Events[key]) => void) | undefined;
@@ -163,74 +166,35 @@ const generateKey = (...args: any) => {
 //   modifiers: string[]
 // ): T extends Event ? EventHandler<T> : T => {
 // =================================================================
+// export const withModifiers = function <E extends Event>(
 export const withModifiers = function <E extends Event>(
   fn: EventHandler<E>,
   modifiers: string[]
 ): EventHandler<E> {
   // =================================================================
   // ): [EventHandler<E>, string[]] => {
-
-  // console.group();
-  // console.log('"withModifiers" args:', {
-  //   fn,
-  //   fnAsString: fn.toString(),
-  //   // @ts-ignore
-  //   __vue_event_modifiers: withModifiers.__proto__.__vue_event_modifiers
-  // });
-  // console.groupEnd();
-
-  // const key = generateKey(fn);
-  // const value = modifiers;
-  // functionModifierMap.set(key, value);
-
-  // const f = <F extends Function>(fn: F) => {
-  //   Object.defineProperty(fn, 'name', { value: 'fh4756gau' });
-
-  //   return fn;
-  // };
-
   const functionWithModifiers = _withModifiers(fn as any, modifiers);
-
-  // functionWithModifiers.toString = function () {
-  //   console.log(11, this);
-
-  //   return this();
-  // };
-
-  const key = generateKey(fn as any, modifiers);
-  console.log('key:', key);
 
   Object.defineProperty(fn, 'name', { writable: true });
   Object.defineProperty(functionWithModifiers, 'name', { writable: true });
   console.log((Math.random() + 1).toString(36).substring(7), Date.now());
+  const i = (Math.random() + 1).toString(36).substring(7);
+  functionWithModifiers.__proto__.displayName = i;
 
-  // // @ts-ignore
-  // functionWithModifiers.name = 'jfui4h5874';
-  // // @ts-ignore
-  // fn.name = 'jfui4h5874';
-  // // @ts-ignore
-  // functionWithModifiers.displayName = 'jfui4h5874';
-  if (functionWithModifiers.__proto__.displayName == null) {
-    functionWithModifiers.__proto__.displayName = generateGuid();
-  }
-  // functionWithModifiers.__proto__.name = 'jfui4h5874';
-  // console.log(777, functionWithModifiers.__proto__.name);
+  const f = new Proxy(functionWithModifiers, {
+    get(target, property, receiver) {
+      // console.log('"f" proxyyyyyy! ', { target, property, receiver });
 
-  // // @ts-ignore
-  // this.modifiers = modifiers;
-
-  // // @ts-ignore
-  // console.log('withModifiers "this"', this);
-
-  // console.group(1);
-  // console.log({ functionWithModifiers });
-  // // // @ts-ignore
-  // // console.log('functionId:', functionId);
-  // // console.log('functionWithModifiersAsString:', functionWithModifiersAsString);
-  // console.groupEnd();
+      // if (property === 'arguments') {
+      if (property === 'modifiers') {
+        return modifiers;
+      }
+    }
+  });
 
   // return [functionWithModifiers, modifiers];
-  return functionWithModifiers;
+  return f;
+  // return functionWithModifiers;
 };
 
 // export const withEventAttributeNameModify = function (
@@ -239,13 +203,13 @@ export const withModifiers = function <E extends Event>(
 export function withEventAttributeNameModify(
   eventObject: RequireOnlyOne<EventObject>
 ) {
-  console.log(111, eventObject);
+  // console.log(111, eventObject);
 
   const eventName = Object.keys(eventObject)[0];
   const eventFunction = Object.values(eventObject)[0];
 
-  // @ts-ignore
-  console.log(234243, eventName, eventObject[eventName]);
+  // // @ts-ignore
+  // console.log(234243, eventName, eventObject[eventName]);
 
   // // @ts-ignore
   // console.log('withEventAttributeNameModify "this"', this.modifiers);
@@ -253,34 +217,23 @@ export function withEventAttributeNameModify(
   // // @ts-ignore
   // const eventFunction = eventObject[eventName] as Function;
 
-  // console.group();
-  // console.log('"withEventAttributeNameModify":', {
-  //   eventObject,
-  //   eventName,
-  //   eventFunction,
-  //   eventFunctionAsString: eventFunction.toString(),
-  //   // @ts-ignore
-  //   __vue_event_modifiers: eventFunction.__proto__.__vue_event_modifiers
-  // });
-  // console.groupEnd();
-
-  // console.log(
-  //   'functionModifierMap:',
-  //   functionModifierMap,
-  //   generateKey(eventFunction)
-  // );
-
   // // @ts-ignore
-  // const functionId = functionWithModifiers[n]();
+  // this.id = generateGuid();
+  const currentInstanceOfComponent = getCurrentInstance();
+  const accessCache = (currentInstanceOfComponent as any).accessCache as Record<
+    string,
+    number
+  >;
 
   console.group(2);
-  console.log('eventName:', eventName);
-  console.log('eventFunction:', eventFunction);
+  // @ts-ignore
+  console.log('withEventAttributeNameModify "this"', this);
+  console.log('currentInstanceOfComponent:', currentInstanceOfComponent);
+  console.log('accessCache:', accessCache);
+  console.log(`eventFunction "${eventName}":`, { eventFunction });
+  // console.log('eventFunction "modifiers":', eventFunction.modifiers);
   console.log('eventFunction "displayName":', eventFunction.displayName);
-  console.log({ eventFunction });
-  // console.log('mods:', mods);
-  // // @ts-ignore
-  // console.log('functionId:', functionId);
+  console.log('eventFunction.__proto__":', { ...eventFunction.__proto__ });
   console.groupEnd();
 
   return eventObject;
