5ac29491642ba652f357f91672415a32e3f10f18
funny-family <maksvadik228@gmail.com>
2022-08-11 22:44:23 +0700

    add "with-event-modifiers" util

---
+++ src/app/shared/utils/with-event-modifiers.ts
@@ -0,0 +1,128 @@
+import { isArrayEmpty } from './is-array-empty';
+import { callTernary } from './call-ternary';
+import { capitalize, Events } from 'vue';
+import { withModifiers, withKeys } from 'vue';
+import type { RequireOnlyOne } from '@/app/shared/types';
+
+type EventObject = {
+  [key in keyof Events]: ((event: Events[key]) => void) | undefined;
+};
+
+const eventModifier = {
+  stop: 'stop',
+  prevent: 'prevent',
+  self: 'self'
+
+  // ctrl : 'ctrl',
+  // shift : 'shift',
+  // alt : 'alt',
+  // meta : 'meta',
+
+  // left : 'left',
+  // middle : 'middle',
+  // right : 'right',
+
+  // exact : 'exact'
+} as const;
+
+const transformativeEventModifier = {
+  capture: 'capture',
+  once: 'once',
+  passive: 'passive'
+} as const;
+
+const isModifierTransformEvent = (modifier: string): boolean =>
+  (
+    [
+      transformativeEventModifier.capture,
+      transformativeEventModifier.once,
+      transformativeEventModifier.passive
+    ] as string[]
+  ).includes(modifier);
+
+const keyAlias = {
+  enter: 'enter',
+  tab: 'tab',
+  delete: 'delete', // (captures both "Delete" and "Backspace" keys)
+  esc: 'esc',
+  space: 'space',
+  up: 'up',
+  down: 'down',
+  left: 'left',
+  right: 'right'
+} as const;
+
+const systemModifier = {
+  ctrl: 'ctrl',
+  alt: 'alt',
+  shift: 'shift',
+  meta: 'meta',
+
+  exact: 'exact'
+} as const;
+
+const mouseButtonModifier = {
+  left: 'left',
+  right: 'right',
+  middle: 'middle'
+} as const;
+
+const nonTransformativeEventModifier = {
+  ...eventModifier,
+  ...systemModifier,
+  ...mouseButtonModifier,
+  ...keyAlias
+};
+
+const eventModifiers = Object.values({
+  ...eventModifier,
+  ...systemModifier,
+  ...mouseButtonModifier
+});
+
+const keyModifiers = Object.values({ ...keyAlias });
+
+export const withEventModifiers = (
+  eventObject: RequireOnlyOne<EventObject>,
+  modifiers: string[]
+) => {
+  const transformativeModifiersList: string[] = [];
+  const eventModifierList: string[] = [];
+  const keyModifierList: string[] = [];
+
+  for (let i = 0; i < modifiers.length; i++) {
+    const modifier = modifiers[i];
+
+    const isCurrentModifierTransformEvent = isModifierTransformEvent(modifier);
+    if (isCurrentModifierTransformEvent) {
+      transformativeModifiersList.push(modifier);
+    }
+
+    const isEventModifier = eventModifiers.includes(
+      modifier as typeof eventModifiers[number]
+    );
+    if (isEventModifier) {
+      eventModifierList.push(modifier);
+    }
+
+    keyModifierList.push(modifier);
+  }
+
+  const inputEventName = Object.keys(eventObject)[0];
+  const inputEventFunction = Object.values(eventObject)[0] as Function;
+
+  const isTransformativeModifierListEmpty = isArrayEmpty(
+    transformativeModifiersList
+  );
+  const tm1 = callTernary({
+    condition: isTransformativeModifierListEmpty,
+    onTruthy: () => '',
+    onFalsy: () => transformativeModifiersList.map(capitalize).join('')
+  });
+  const outputEventName = `${inputEventName}${tm1}`;
+  // const outputEventFunction = callTernary({
+  //   condition
+  // })
+
+  return {};
+};
