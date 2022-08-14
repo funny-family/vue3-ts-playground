1bc7dccbd9494ba8bc356bfad5eec0d71d3555ec
funny-family <maksvadik228@gmail.com>
2022-08-14 23:36:28 +0700

    update main store file

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -17,6 +17,35 @@
 // https://soshace.com/building-web-apps-with-vue-3-composition-api-typescript-vuex4-0/ !!!
 // https://dev.to/shubhadip/vue-3-vuex-4-modules-typescript-2i2o
 
-import { createPinia } from 'pinia';
+import { createPinia, defineStore, StoreDefinition } from 'pinia';
+import type { DefineSetupStoreOptions } from 'pinia';
+import type {
+  _ExtractActionsFromSetupStore,
+  _ExtractGettersFromSetupStore,
+  _ExtractStateFromSetupStore
+} from './types';
 
 export const store = createPinia();
+
+/**
+ * Creates a "readonly" `useStore` function that retrieves the store instance
+ *
+ * @param id - id of the store (must be unique)
+ * @param storeSetup - function that defines the store
+ * @param options - extra options
+ */
+export const defineReadOnlyStore = defineStore as <Id extends string, SS>(
+  id: Id,
+  storeSetup: () => SS,
+  options?: DefineSetupStoreOptions<
+    Id,
+    _ExtractStateFromSetupStore<SS>,
+    _ExtractGettersFromSetupStore<SS>,
+    _ExtractActionsFromSetupStore<SS>
+  >
+) => StoreDefinition<
+  Id,
+  Readonly<_ExtractStateFromSetupStore<SS>>,
+  Readonly<_ExtractGettersFromSetupStore<SS>>,
+  Readonly<_ExtractActionsFromSetupStore<SS>>
+>;
