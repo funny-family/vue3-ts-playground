5393d39e86a390040cc94a3b76bd0200395b897a
funny-family <maksvadik228@gmail.com>
2022-08-18 23:29:51 +0700

    update "text-field" component

--- src/app/shared/components/text-field/text-field.render.tsx
+++ src/app/shared/components/text-field/text-field.render.tsx
@@ -1,18 +1,18 @@
 import type { TextFieldUnwrappedBindings } from './text-field.setup';
 import type { RenderFunction } from '@/app/shared/types/component/render';
-import { styles } from './styles/text-field.styles';
 /**
  * @see https://segmentfault.com/a/1190000041616822
  */
 import { normalizeClass } from 'vue';
+import { styles } from './styles/text-field.styles';
 
 export const render: RenderFunction<TextFieldUnwrappedBindings> = function () {
   const { props, context, onInput, $, shallowObj, deepObj } = this;
-  const { class: classAttr, style: styleAttr, ...restAttrs } = context.attrs;
-  const uid = $.uid;
+  const { class: classAttr, style, ...forwardElementAttrs } = context.attrs;
   const className = normalizeClass([classAttr, styles.textField]);
-  const style = styleAttr;
+  const type = forwardElementAttrs.type || 'text';
   const value = props.modelValue;
+  const uid = $.uid;
 
   return (
     <div class={className} style={style} data-component-root-el-id={uid}>
@@ -21,9 +21,9 @@ export const render: RenderFunction<TextFieldUnwrappedBindings> = function () {
       )}
 
       <input
-        {...restAttrs}
+        {...forwardElementAttrs}
         class={styles.textField__input}
-        type="text"
+        type={type}
         value={value}
         onInput={onInput}
         data-component-forward-el-id={uid}
