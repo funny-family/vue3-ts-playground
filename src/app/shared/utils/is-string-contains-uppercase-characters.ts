5f90e0b41743d0a919ea1181b247727d053fc208
funny-family <maskvadik228@gmail.com>
2022-12-13 09:48:57 +0700

    update util

--- src/app/shared/utils/is-string-contains-uppercase-characters.ts
+++ src/app/shared/utils/is-string-contains-uppercase-characters.ts
@@ -1,5 +1,6 @@
 /**
  * @see https://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip
+ * @see https://stackoverflow.com/a/69874941
  *
  * @description
  * adada
