ec104a3ddcef382117431a0b988df18d19ef4010
funny-family <maksvadik228@gmail.com>
2022-12-23 19:58:17 +0700

    update "use-attrs" composable

--- src/app/shared/composables/use-attrs.composable.ts
+++ src/app/shared/composables/use-attrs.composable.ts
@@ -11,6 +11,43 @@ import type {
 import { computed, toRefs, shallowRef, reactive } from 'vue';
 import { isOn } from '@vue/shared';
 
+// =========================================================================================
+// https://catchts.com/union-array
+// credits goes to https://stackoverflow.com/a/50375286
+// https://matthias-falk.medium.com/tuple-union-conversions-in-typescript-77fef29cd6e6
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
+// Converts union to overloaded function
+type UnionToOvlds<U> = UnionToIntersection<
+  U extends any ? (f: U) => void : never
+>;
+
+type PopUnion<U> = UnionToOvlds<U> extends (a: infer A) => void ? A : never;
+
+type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true;
+
+// Finally me)
+type UnionToArray<T, A extends unknown[] = []> = IsUnion<T> extends true
+  ? UnionToArray<Exclude<T, PopUnion<T>>, [PopUnion<T>, ...A]>
+  : [T, ...A];
+// =========================================================================================
+
+// =========================================================================================
+type FilterStartsWith<
+  Set,
+  Needle extends string
+> = Set extends `${Needle}${infer _X}` ? Set : never;
+
+type FilterNotStartingWith<
+  Set,
+  Needle extends string
+> = Set extends `${Needle}${infer _X}` ? never : Set;
+// =========================================================================================
+
 // https://stackoverflow.com/questions/43909566/get-keys-of-a-typescript-interface-as-array-of-strings
 
 // https://blog.wax-o.com/2015/05/an-alternative-to-if-else-and-switch-in-javascript/
@@ -36,90 +73,43 @@ const isDataAttribute = <T extends string>(key: T) =>
   /^(data-)[a-z]+/.test(key);
 
 // https://github.com/microsoft/TypeScript/issues/13298
-
-// ========================================================
-// type TupleHead<Tuple extends readonly unknown[]> = Tuple extends [
-//   infer HeadElement,
-//   ...(readonly unknown[])
-// ]
-//   ? HeadElement
-//   : never;
-
-// type TupleTail<Tuple extends readonly unknown[]> = Tuple extends [
-//   unknown,
-//   ...infer TailElements
-// ]
-//   ? TailElements
-//   : never;
-
-type TuplePrepend<Tuple extends readonly unknown[], NewElement> = [
-  NewElement,
-  ...Tuple
-];
-
-type Consumer<Value> = (value: Value) => void;
-
-type IntersectionFromUnion<Union> = (
-  Union extends unknown ? Consumer<Union> : never
-) extends Consumer<infer ResultIntersection>
-  ? ResultIntersection
-  : never;
-
-type OverloadedConsumerFromUnion<Union> = IntersectionFromUnion<
-  Union extends unknown ? Consumer<Union> : never
->;
-
-type UnionLast<Union> = OverloadedConsumerFromUnion<Union> extends (
-  a: infer A
-) => void
-  ? A
-  : never;
-
-type UnionExcludingLast<Union> = Exclude<Union, UnionLast<Union>>;
-
-type TupleFromUnionRec<
-  RemainingUnion,
-  CurrentTuple extends readonly unknown[]
-> = [RemainingUnion] extends [never]
-  ? CurrentTuple
-  : TupleFromUnionRec<
-      UnionExcludingLast<RemainingUnion>,
-      TuplePrepend<CurrentTuple, UnionLast<RemainingUnion>>
-    >;
-
-export type TupleFromUnion<Union> = TupleFromUnionRec<Union, []>;
-// // ========================================================
-
-// // ========================================================
-// // https://stackoverflow.com/questions/55127004/how-to-transform-union-type-to-tuple-type
-// // oh boy don't do this
-// type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-//   k: infer I
-// ) => void
-//   ? I
-//   : never;
-// type LastOf<T> = UnionToIntersection<
-//   T extends any ? () => T : never
-// > extends () => infer R
-//   ? R
-//   : never;
-
-// // TS4.0+
-// type Push<T extends any[], V> = [...T, V];
-
-// // TS4.1+
-// type TuplifyUnion<
-//   T,
-//   L = LastOf<T>,
-//   N = [T] extends [never] ? true : false
-// > = true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>;
-// // ========================================================
-
+// https://stackoverflow.com/questions/55127004/how-to-transform-union-type-to-tuple-type
 // https://stackoverflow.com/questions/51804810/how-to-remove-fields-from-a-typescript-interface-via-extension
 
-type HTMLAttributeKey = keyof E;
+// =========================================================================================
+type OnlyHTMLAttributes = Pick<
+  HTMLAttributes,
+  FilterNotStartingWith<keyof HTMLAttributes, 'on' | 'aria-' | 'data-' | 'v-'>
+>;
 
-type HTMLAttributesTyple = TupleFromUnion<HTMLAttributeKey>;
+type OnlyEventAttributes<T extends HTMLAttributes> = Pick<
+  T,
+  FilterStartsWith<keyof T, 'on'>
+>;
+
+type OnlyAriaAttributes<T extends HTMLAttributes> = Pick<
+  T,
+  FilterStartsWith<keyof T, 'aria-'>
+>;
+
+type OnlyDataAttributes<T extends HTMLAttributes> = Pick<
+  T,
+  FilterStartsWith<keyof T, 'data-'>
+>;
+
+type OnlyElAttributes<T extends HTMLAttributes> = Omit<
+  T,
+  | keyof OnlyHTMLAttributes
+  | keyof OnlyEventAttributes<T>
+  | keyof OnlyAriaAttributes<T>
+  | keyof OnlyDataAttributes<T>
+  | FilterStartsWith<keyof T, 'v-'>
+>;
+
+type HTMLAttributesTyple = UnionToArray<keyof OnlyHTMLAttributes>;
+
+// Union to Tuple
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00730-hard-union-to-tuple/README.md
 
 const htmlAttributes: HTMLAttributesTyple = [
   'innerHTML',
@@ -181,68 +171,12 @@ const attr = {
   el: {}
 };
 
-type FilterHTMLAttributes<T extends HTMLAttributes> = {
-  [P in keyof T]: StartsWith<P, HTMLAttributeKey> extends true ? T[P] : never;
-};
-type OnlyHTMLAttributes<T extends HTMLAttributes> = OmitByType<
-  FilterHTMLAttributes<T>,
-  undefined
->;
-
-type FilterEventAttributes<T extends HTMLAttributes> = {
-  [P in keyof T]: StartsWith<P, 'on'> extends true ? T[P] : never;
-};
-type OnlyEventAttributes<T extends HTMLAttributes> = OmitByType<
-  FilterEventAttributes<T>,
-  undefined
->;
-
-type FilterAriaAttributes<T extends HTMLAttributes> = {
-  [P in keyof T]: StartsWith<P, 'aria-'> extends true ? T[P] : never;
-};
-type OnlyAriaAttributes<T extends HTMLAttributes> = OmitByType<
-  FilterAriaAttributes<T>,
-  undefined
->;
-
-type FilterDataAttributes<T extends HTMLAttributes> = {
-  [P in keyof T]: StartsWith<P, 'data-'> extends true ? T[P] : never;
-};
-type OnlyDataAttributes<T extends HTMLAttributes> = OmitByType<
-  FilterDataAttributes<T>,
-  undefined
->;
-
-type OmitableElKeys<T extends HTMLAttributes> =
-  | keyof OnlyHTMLAttributes<T>
-  | keyof OnlyEventAttributes<T>
-  | keyof OnlyAriaAttributes<T>
-  | keyof OnlyDataAttributes<T>;
-type OnlyElAttributes<T extends HTMLAttributes> = Omit<T, OmitableElKeys<T>>;
-
-type FilerDirective<T extends Object> = {
-  [P in keyof T]: StartsWith<P, 'v-'> extends true ? never : T[P];
-};
-type WithoutDirective<T extends Object> = OmitByType<
-  FilerDirective<T>,
-  undefined
->;
-
-type E = WithoutDirective<
-  Omit<
-    HTMLAttributes,
-    | keyof OnlyEventAttributes<HTMLAttributes>
-    | keyof OnlyAriaAttributes<HTMLAttributes>
-    | keyof OnlyDataAttributes<HTMLAttributes>
-  >
->;
-
 type ResolvedAttrs<T extends HTMLAttributes> = {
-  html: OnlyHTMLAttributes<T>;
-  event: OnlyEventAttributes<T>;
-  aria: OnlyAriaAttributes<T>;
-  data: OnlyDataAttributes<T>;
-  el: WithoutDirective<OnlyElAttributes<T>>;
+  html: Partial<OnlyHTMLAttributes>;
+  event: Partial<OnlyEventAttributes<T>>;
+  aria: Partial<OnlyAriaAttributes<T>>;
+  data: Partial<OnlyDataAttributes<T>>;
+  el: Partial<OnlyElAttributes<T>>;
 };
 
 const resolveAttrs = <T extends HTMLAttributes>(attrs: T): ResolvedAttrs<T> => {
@@ -251,31 +185,31 @@ const resolveAttrs = <T extends HTMLAttributes>(attrs: T): ResolvedAttrs<T> => {
   for (const key in attrs) {
     switch (true) {
       case isAriaAttribute(key): {
-        (attrsRecord.aria as Record<string, any>)[key] = attrs[key];
+        (attrsRecord.aria as any)[key] = attrs[key];
 
         break;
       }
 
       case isDataAttribute(key): {
-        (attrsRecord.data as Record<string, any>)[key] = attrs[key];
+        (attrsRecord.data as any)[key] = attrs[key];
 
         break;
       }
 
       case isHtmlAttribute(key): {
-        (attrsRecord.html as Record<string, any>)[key] = attrs[key];
+        (attrsRecord.html as any)[key] = attrs[key];
 
         break;
       }
 
       case isOn(key): {
-        (attrsRecord.event as Record<string, any>)[key] = attrs[key];
+        (attrsRecord.event as any)[key] = attrs[key];
 
         break;
       }
 
       default: {
-        (attrsRecord.el as Record<string, any>)[key] = attrs[key];
+        (attrsRecord.el as any)[key] = attrs[key];
 
         break;
       }
