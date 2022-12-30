cf902e9f328ede4bb4cc9dc523a3f8de33701164
funny-family <maksvadik228@gmail.com>
2022-12-30 20:24:55 +0700

    update types of "setup"

--- src/app/shared/types/component/setup.ts
+++ src/app/shared/types/component/setup.ts
@@ -1,11 +1,14 @@
-import type { EmitsOptions, SetupContext, Slots } from 'vue';
+import type { EmitsOptions, SetupContext, Slots, HTMLAttributes } from 'vue';
 import type { ShallowUnwrapRef } from '@vue/reactivity';
 import type { Data, AnyFunction } from '@/app/shared/types';
 import type { EmitFunction } from '@/app/shared/types/component/emits';
 
 // Extended "SetupContext" type.
-export interface SetupCtx<E = EmitsOptions, A = Data, S = Slots>
-  extends Omit<SetupContext<E>, 'attrs' | 'slots' | 'emit'> {
+export interface SetupCtx<
+  A extends Data | HTMLAttributes,
+  S extends Slots,
+  E extends EmitsOptions
+> extends Omit<SetupContext<E>, 'attrs' | 'slots' | 'emit'> {
   attrs: A;
   slots: S;
   emit: EmitFunction<E>;
@@ -14,10 +17,10 @@ export interface SetupCtx<E = EmitsOptions, A = Data, S = Slots>
 export type PropsOfSetupFunction<T = {}> = Readonly<T>;
 
 export type ContextOfSetupFunction<
-  E = EmitsOptions,
-  A = Data,
-  S = Slots
-> = SetupCtx<E> & {
+  A extends Data | HTMLAttributes,
+  S extends Slots,
+  E extends EmitsOptions
+> = SetupCtx<A, S, E> & {
   attrs: A;
   slots: S;
   emit: EmitFunction<E>;
