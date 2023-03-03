c15132055e4c7e2d0cd85f89d5c28bd31fb1a6f5
funny-family <maksvadik228@gmail.com>
2023-03-03 23:56:32 +0700

    update "counter" store

--- src/app/store/stores/couter.store.ts
+++ src/app/store/stores/couter.store.ts
@@ -1,28 +1,6 @@
 import { storeId } from './../index';
 import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
-import type { ReadonlyStoreSetup } from './types';
-
-// export const id = 'counter' as const;
-// const storeSetup = () => {
-//   const counter = ref(0);
-//   const doubleCounter = computed(() => counter.value * 2);
-
-//   const increment = () => {
-//     counter.value++;
-//   };
-
-//   return {
-//     counter,
-//     doubleCounter,
-//     increment
-//   };
-// };
-
-// export const useCounterStore = defineStore(
-//   id,
-//   storeSetup as ReadonlyStoreSetup<typeof storeSetup>
-// );
 
 export const useCounterStore = defineStore(storeId.counter, () => {
   const counter = ref(0);
