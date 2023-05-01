3023fa1dcecf7cf3b555a47aae3fcea2ecb167e0
funny-family <maksvadik228@gmail.com>
2023-05-01 23:50:51 +0700

    update types of "render" function

--- src/app/shared/types/component/render.ts
+++ src/app/shared/types/component/render.ts
@@ -1,24 +1,24 @@
 import type { VNodeTypes, ComponentPublicInstance } from 'vue';
 import type { Data, JSXElement } from '@/app/shared/types';
 
-type D = Data<undefined>;
-
 type RenderFunctionReturnValue = VNodeTypes | JSXElement;
 
 export type RenderFunction<
   This,
-  Ctx = D,
-  Cache = any[],
-  $Props = D,
-  $Setup = This,
-  $Data = D,
-  $Options = any
+  $Context = This, // 0
+  $Cache = any[], // 1
+  $Props = Record<string, any>, // 2
+  $Setup = This, // 3
+  $Data = {}, // 4
+  $Options = This // 5
+  // custom values
+  //
 > = (
   this: This & ComponentPublicInstance,
-  ctx: Ctx & ComponentPublicInstance,
-  cache: Cache,
-  $props: $Props,
-  $setup: $Setup,
-  $data: $Data,
-  $options: $Options
+  $context: $Context & ComponentPublicInstance, // 0
+  $cache: $Cache, // 1
+  $props: $Props, // 2
+  $setup: $Setup, // 3
+  $data: $Data, // 4
+  $options: $Options & ComponentPublicInstance // 5
 ) => RenderFunctionReturnValue;
